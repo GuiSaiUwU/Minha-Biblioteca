0ddd7b8eb3f55f7bc1061d75210e8b117136d524
GuiSaiUwU <107002177+GuiSaiUwU@users.noreply.github.com>
2026-05-18 23:23:05 -0300

    feat(backend): Inicializado

---
+++ biblioteca-backend/src/test/java/com/guilherme_augusto/biblioteca/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme_augusto.biblioteca.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
